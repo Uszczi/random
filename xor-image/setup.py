262c12f756779fd87e994ad3a2f05179d7e838b5
Uszczi <mateuszpapuga24@gmail.com>
2021-11-01 15:51:54 +0100

    fix setup.py

--- xor-image/setup.py
+++ xor-image/setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
     name="xor-scrambling",
-    package_dir={"": "src"},
+    # package_dir={"": "src"},
+    packages=["src"],
     version="0.1",
     entry_points="""
         [console_scripts]
-        python-xor=main:main
+        python-xor=src.main:main
     """,
 )
