35696a686a0d611495a7006e78dd56151c3dc1fd
Uszczi <mateuszpapuga24@gmail.com>
2021-11-01 13:07:41 +0100

    split images to load and save

--- xor-image/gui.py
+++ xor-image/gui.py
@@ -1,5 +1,6 @@
 import sys
-
+from dataclasses import field, dataclass
+from typing import Any
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QImage, QPixmap
 from PyQt6.QtWidgets import (
@@ -15,17 +16,51 @@ from PyQt6.QtWidgets import (
     QWidget,
 )
 
-from core import xor
+import core
 
-pp = {}
-
-# TODO add global path names
-# TODO add global result image
-# TODO add save button button
+# TODO add dialog box for success
+# TODO Disable save button at start
+# TODO add injector
 
 
-class Image(QWidget):
-    def __init__(self, image_text, load_button=False):
+@dataclass
+class GlobalState:
+    image_path: str = ""
+    key_path: str = ""
+    widget_layout: Any = field(init=False)
+    result_image: Any = field(init=False)
+
+    def set_image_path(self, path: str) -> None:
+        self.image_path = path
+        self._make_encryption()
+
+    def set_key_path(self, path: str) -> None:
+        self.key_path = path
+        self._make_encryption()
+
+    def set_result_widget(self, widget):
+        self.widget_layout = widget
+
+    def _make_encryption(self):
+        if not self.image_path or not self.key_path:
+            return
+
+        image = core.xor(self.image_path, self.key_path)
+        h, w, _ = image.shape
+
+        def f(_):
+            return
+
+        result_image = QImage(
+            image.data, w, h, 3 * w, QImage.Format.Format_RGB888, f, f
+        )
+        self.widget_layout.set_pixmap(result_image)
+        self.widget_layout.activate_button()
+        self.result_image = image
+
+
+class LoadImage(QWidget):
+    def __init__(self, image_text, button_function):
         super().__init__()
 
         self.image_label = QLabel(image_text)
@@ -38,27 +73,17 @@ class Image(QWidget):
         layout = QVBoxLayout()
         layout.addWidget(self.image_label)
 
-        button_text = "Load image" if load_button else ""
-        self.button = QPushButton(button_text)
+        self.button = QPushButton("Load image")
+        self.button.clicked.connect(lambda: self.open(button_function))
         layout.addWidget(self.button)
-        if load_button:
-            self.button.clicked.connect(lambda: self.open(image_text))
-        else:
-            pp["dd"] = self
-
         layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.setLayout(layout)
 
-    def open(self, image_text):
+    def open(self, function) -> None:
         file_name, _ = QFileDialog.getOpenFileName(
             self, "QFileDialog.getOpenFileName()", "", "Images (*.png *.jpeg *.jpg)"
         )
-        #         file_name, _ = QFileDialog.getSaveFileName(
-        #     self,
-        #     "QFileDialog.getSaveFileName()",
-        #     "",
-        #     "Images (*.png *.jpeg *.jpg)",
-        # )
+
         if file_name:
             image = QImage(file_name)
             if image.isNull():
@@ -67,37 +92,54 @@ class Image(QWidget):
                 )
                 return
             self.image_label.setPixmap(QPixmap.fromImage(image))
+            function(file_name)
 
-            if "key" in image_text:
-                pp["key_path"] = file_name
-            else:
-                pp["image_path"] = file_name
 
-            if pp.get("image_path") and pp.get("key_path"):
-                result = xor(pp.get("image_path"), pp.get("key_path"))
-                h, w, d = result.shape
+class ResultImage(QWidget):
+    def __init__(self, global_state):
+        super().__init__()
 
-                def f(_):
-                    return
+        self.image_label = QLabel("Result image")
+        self.image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.image_label.setStyleSheet("background: #7c9c85")
+        self.image_label.setSizePolicy(
+            QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding
+        )
+        self.setMaximumSize(500, 500)
+        layout = QVBoxLayout()
+        layout.addWidget(self.image_label)
 
-                result_image = QImage(
-                    result.data,
-                    w,
-                    h,
-                    3 * w,
-                    QImage.Format.Format_RGB888,
-                    f,
-                    f,
-                )
-                pp["dd"].image_label.setPixmap(QPixmap.fromImage(result_image))
+        self.button = QPushButton("Save image")
+        self.button.clicked.connect(lambda: self.save(global_state))
+        self.button.setDisabled(True)
+        layout.addWidget(self.button)
+        layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.setLayout(layout)
+
+    def save(self, global_state) -> None:
+        file_name, _ = QFileDialog.getSaveFileName(
+            self,
+            "QFileDialog.getSaveFileName()",
+            "",
+            "Images (*.png *.jpeg *.jpg)",
+        )
+        core.save(file_name, global_state.result_image)
+
+    def set_pixmap(self, pixmap):
+        self.image_label.setPixmap(QPixmap.fromImage(pixmap))
+
+    def activate_button(self):
+        self.button.setDisabled(False)
 
 
 class MainWidget(QWidget):
-    def __init__(self):
+    def __init__(self, global_state: GlobalState):
         super().__init__()
-        self.load_image = Image("Image to process", load_button=True)
-        self.key_image = Image("Image as key", load_button=True)
-        self.result_image = Image("Result")
+        self.global_state = global_state
+        self.load_image = LoadImage("Image to process", global_state.set_image_path)
+        self.key_image = LoadImage("Image as key", global_state.set_key_path)
+        self.result_image = ResultImage(global_state)
+        global_state.set_result_widget(self.result_image)
 
         self.h_layout = QHBoxLayout()
         self.h_layout.addWidget(self.load_image)
@@ -110,7 +152,8 @@ class MainWidget(QWidget):
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        main_widget = MainWidget()
+        global_state = GlobalState()
+        main_widget = MainWidget(global_state)
         self.setCentralWidget(main_widget)
         self.resize(1600, 900)
 
